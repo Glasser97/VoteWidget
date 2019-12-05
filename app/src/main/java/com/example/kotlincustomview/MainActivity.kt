278260e8ef6a063055156e6275a029bcd7f47249
graysonzeng <graysonzeng@futunn.com>
2019-12-05 16:50:42 +0800

    通过重写performClick方法,添加了取消投票点击事件

--- app/src/main/java/com/example/kotlincustomview/MainActivity.kt
+++ app/src/main/java/com/example/kotlincustomview/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.kotlincustomview
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.vote_widget_layout.*
 import kotlinx.coroutines.*
@@ -15,19 +17,26 @@ class MainActivity : VoteButton.VoteClickListener, AppCompatActivity() {
         coroutineScope.launch{
             var displaySource = withContext(Dispatchers.IO){
                 //delay(2000)
-                VoteWidgetDisplaySource("看好","不看好",29,71,10000,false,false,false)
+                VoteWidgetDisplaySource("看好","不看好",29,71,1000,false,false,false)
             }
             vote_widget.fill(displaySource)
         }
-
         vote_widget.setOnClickListener{
-            coroutineScope.launch{
-                var displaySource = withContext(Dispatchers.IO){
-                    delay(1000)
-                    VoteWidgetDisplaySource("看好","不看好",29,71,10000,false,false,false)
+            AlertDialog.Builder(this)
+                .setMessage("确定取消投票吗?")
+                .setCancelable(true)
+                .setNegativeButton("取消",null)
+                .setPositiveButton("确定") { _, _ ->
+                    coroutineScope.launch{
+                        var displaySource = withContext(Dispatchers.IO){
+                            delay(1500)
+                            VoteWidgetDisplaySource("看好","不看好",29,71,1000,false,false,false)
+                        }
+                        vote_widget.fill(displaySource)
+                    }
                 }
-                vote_widget.fill(displaySource)
-            }
+                .create()
+                .show()
         }
         //var mDisplaySource = VoteWidgetDisplaySource("看好","不看好",2,2,10000,false,false,false)
         //vote_widget.fill(mDisplaySource)
